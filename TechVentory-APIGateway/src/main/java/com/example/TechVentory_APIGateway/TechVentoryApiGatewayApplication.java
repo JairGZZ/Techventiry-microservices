fcc3330e0faef9617f53925c9c53cd8887e5be6f
JairGZZ <jairprox1234@gmail.com>
2025-09-30 00:13:09 -0500

    init: add initial setup for api-gateway

---
+++ TechVentory-APIGateway/src/main/java/com/example/TechVentory_APIGateway/TechVentoryApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.example.TechVentory_APIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class TechVentoryApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechVentoryApiGatewayApplication.class, args);
+	}
+
+}
