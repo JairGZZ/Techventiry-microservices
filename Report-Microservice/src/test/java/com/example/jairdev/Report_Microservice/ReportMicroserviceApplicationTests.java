5b36c7d3f2c3b032c576924fb70b4f8f7dc216d6
JairGZZ <jairprox1234@gmail.com>
2025-09-30 00:12:16 -0500

    init: add initial setup for report-microservice

---
+++ Report-Microservice/src/test/java/com/example/jairdev/Report_Microservice/ReportMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jairdev.Report_Microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
